0c4d4b9c7282a35b379a3a752ce3de9e6ba9fbb4
mefius5 <maciejkowalski1990@o2.pl>
2018-10-21 00:35:39 +0200

    set session in login and register form

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,6 +6,7 @@ require_once 'config/config.php';
 //require_once 'libraries/controller.php';
 //require_once 'libraries/database.php';
 require_once 'helpers/url_helper.php';
+require_once 'helpers/session_helper.php';
 
 //Autoload Core Libraries
 spl_autoload_register(function($className){
--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -63,6 +63,7 @@
                     
                     // Register User
                     if($this->userModel->register($data)){
+                        flash('register_success', 'You are registered and can log in');
                         redirect('users/login');
                     }else{
                         die('Somethin wrong');
@@ -117,10 +118,28 @@
                     $data['password_err'] = 'Please enter password';
                 } 
                 
+                //Check for user email
+                if($this->userModel->findUserByEmail($data['email'])){
+                    //User found
+                }else{
+                    $data['email_err'] = 'No user found';
+                }
+                
                 // Make sure errors are empty
                 if(empty($data['email_err']) &&  empty($data['password_err'])){
                     //Validated
-                    die('SUCCESS');
+                    //Check and set logged in user
+                    $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+                    
+                    if($loggedInUser){
+                        //Create Session
+                        $this->createUserSession($loggedInUser);
+                        die('SUCCESS');
+                    }else{
+                        $data['password_err'] = 'Password incorrect';
+                        
+                        $this->view('users/login', $data);
+                    }
                 }else{
                    // Load view with errors
                     $this->view('users/login', $data);
@@ -140,5 +159,12 @@
                 ];
             }
             $this->view('users/login', $data);
+        }
+        
+        public function createUserSession($user){
+            $_SESSION['user_id'] = $user->id;
+            $_SESSION['user_email'] = $user->email;
+            $_SESSION['user_name'] = $user->name;
+            redirect('pages/index');
         }
     }
