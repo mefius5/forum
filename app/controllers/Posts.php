e1cde56569defa9abba7b75bc8d351fa07c01be9
mefius5 <maciejkowalski1990@o2.pl>
2018-11-19 00:24:53 +0100

    update the posts controller and model

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,10 +1,12 @@
 <?php
     class Posts extends Controller{
         public function __construct(){
-//            if(!$isLoggedIn){
+//            if(!isLoggedIn){
 //                redirect('user/login');
 //            }
             $this->postModel = $this->model('Post');
+            $this->userModel = $this->model('User');
+            
         }
         
         public function index(){
@@ -19,11 +21,61 @@
         }
         
         public function add(){
-            $data =[
-                'title' => '',
-                'body' => ''
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                //Sanitize post array
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+                $data =[
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_err' => '',
+                    'body_err' => ''
+                ];
+                                    
+                // Validate title
+                if(empty($data['title'])){
+                    $data['title_err'] = 'Please enter title';
+                }
+                
+                if(empty($data['body'])){
+                    $data['body_err'] = 'Please enter body text';
+                }
+                
+                //Make sure no errors
+                if(empty($data['title_err']) && empty($data['body_err'])){
+                    //Validated
+                    if($this->postModel->addPost($data)){
+                        flash('post_message', 'Post Added');
+                        redirect('posts');
+                    }else{
+                        die('Somethig went wrong');
+                    }
+                }else{
+                    //load view with errors
+                    $this->view('posts/add', $data);
+                }
+            
+                
+            }else{
+                $data =[
+                    'title' => '',
+                    'body' => ''
+                ];
+
+                $this->view('posts/add', $data);
+            }
+            
+        }
+        
+        public function show($id){
+            $post = $this->postModel->getPostById($id);
+            $user = $this->userModel->getUserById($post->user_id);
+            
+            $data = [
+                'post' => $post,
+                'user' => $user
             ];
             
-            $this->view('posts/add', $data);
+            $this->view('posts/show', $data);
         }
     }
