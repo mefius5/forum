65c0ecca780d0455df176a0859e504496a213f33
mefius5 <maciejkowalski1990@o2.pl>
2018-11-20 00:54:23 +0100

    edit and delete posts controller and model

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -78,4 +78,80 @@
             
             $this->view('posts/show', $data);
         }
+        
+        public function edit($id){
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                //Sanitize post array
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+                $data =[
+                    'id' => $id,
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_err' => '',
+                    'body_err' => ''
+                ];
+                                    
+                // Validate title
+                if(empty($data['title'])){
+                    $data['title_err'] = 'Please enter title';
+                }
+                
+                if(empty($data['body'])){
+                    $data['body_err'] = 'Please enter body text';
+                }
+                
+                //Make sure no errors
+                if(empty($data['title_err']) && empty($data['body_err'])){
+                    //Validated
+                    if($this->postModel->updatePost($data)){
+                        flash('post_message', 'Post Updated');
+                        redirect('posts');
+                    }else{
+                        die('Somethig went wrong');
+                    }
+                }else{
+                    //load view with errors
+                    $this->view('posts/edit', $data);
+                }
+            
+                
+            }else{
+                // Get existing post from model
+                $post = $this->postModel->getPostById($id);
+                
+                //Check for owner
+                if($post->user_id != $_SESSION['user_id']){
+                    redirect('posts');
+                }
+                $data =[
+                    'id' => $id,
+                    'title' => $post->title,
+                    'body' => $post->body
+                ];
+
+                $this->view('posts/edit', $data);
+            }
+            
+        }
+        
+        public function delete($id){
+         // Get existing post from model
+            $post = $this->postModel->getPostById($id);
+
+            //Check for owner
+            if($post->user_id != $_SESSION['user_id']){
+                redirect('posts');
+            
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                if($this->postModel->deletePost($id)){
+                    flash('post_message', 'Post Removed');
+                    redirect('posts');
+                }else{
+                    die('Something went wrong');
+                }
+            } else {
+                redirect('posts');
+            }
+        }
     }
